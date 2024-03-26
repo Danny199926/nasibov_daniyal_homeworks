c6790a172c4478ebca7b5cd4641198ec76a8b797
danny <nasdanny50@gmail.com>
2024-03-26 10:24:55 +0300

    add new file task_3.py

---
+++ homework_1/task_3.py
@@ -0,0 +1,13 @@
+# Даны два действительных числа. Найти среднее арифметическое и
+# среднее геометрическое этих чисел.
+
+#ariphmetic
+numbers = [5, 5]
+average = sum(numbers) / len(numbers)
+print(average)
+
+import math
+a = 5
+b = 5
+result = math.sqrt(a * b)
+result2 = (a + b) / 2
