8768611456c69d386de53cb5945ed22fe28fcd6b
Danny199926 <164774756+Danny199926@users.noreply.github.com>
2024-05-07 19:06:06 +0300

    Update task_5.py

--- homework_1/task_5.py
+++ homework_1/task_5.py
@@ -1,7 +1,11 @@
 # Вычислить сумму цифр случайного трёхзначного числа.
 
 import random
-
-number = random.randint(100, 999)
-print(number)
-print(sum((number // 100, number // 10 % 10, number)))
+ran_num = random.randint(100,999)
+print("Случайное число:", ran_num)
+ran_num = str(ran_num)
+ran_num1 = int(ran_num[0])
+ran_num2 = int(ran_num[1])
+ran_num3 = int(ran_num[2])
+sum_ran_num = ran_num1 + ran_num2 + ran_num3
+print("Сумма:", sum_ran_num)
