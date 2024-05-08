e4155773c78907560135d40e44e88e77ace1350f
Danny199926 <164774756+Danny199926@users.noreply.github.com>
2024-05-08 10:41:30 +0300

    Update task_4.py

--- homework_5/task_4.py
+++ homework_5/task_4.py
@@ -7,7 +7,7 @@ n = int(input('Введите второе число: '))
 if m < n:
     for i in range(m, n + 1):
         print(i)
-    else:
-        for i in range(m, n - 1, -1):
-            print(i)
+else:
+    for i in range(m, n - 1, -1):
+        print(i)
 
