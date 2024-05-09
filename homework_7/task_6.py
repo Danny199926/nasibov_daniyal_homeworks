86ee77cb723da27f409726a69d5468ae3b9c878a
Danny199926 <164774756+Danny199926@users.noreply.github.com>
2024-05-09 19:24:34 +0300

    Update task_6.py

--- homework_7/task_6.py
+++ homework_7/task_6.py
@@ -16,3 +16,8 @@ x[index_of_min], x[index_of_max] = x[index_of_max], x[index_of_min]
 print(x)
 
 
+list6 = [-5, -3, 8, -2, 10, 15, 3, 9, 200, 909, -1099]
+min_index = list6.index(min(list6))
+max_index = list6.index(max(list6))
+list6[min_index], list6[max_index] = list6[max_index], list6[min_index]
+print(list6)
