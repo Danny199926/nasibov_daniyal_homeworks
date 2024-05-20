f2d3068b4a50850e5c438966c6e6c9dc84a24341
Danny199926 <164774756+Danny199926@users.noreply.github.com>
2024-05-20 10:44:43 +0300

    Update task_7.py

--- homework_9/task_7.py
+++ homework_9/task_7.py
@@ -2,7 +2,7 @@
 # созданных множеств. Выполнить операцию пересечения созданных множеств.
 
 set1 = {1, 2, 3, 4, 5}
-set2 = 6, 7, 3, 3, 8, 9
+set2 = 6, 7, 8, 9, 10
 union = set1.union(set(set2))
 print(union)
 intersection = set1.intersection(set2)
